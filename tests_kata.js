51767c8cead0ee7214a2e1991712c045caa64a4b
berenice bourgon <berenice.bourgon@gmail.com>
2020-05-23 10:02:17 +0200

    initialize repository

---
+++ tests_kata.js
@@ -0,0 +1,12 @@
+/* 
+Write a test suite then a code in Javascript to do this task:
+
+Simplify a directory path from a string as input.
+Example:
+Input : /a/./b/../../c/
+Output : /c
+
+Write a README to show us how to run your tests.
+
+If something isn't clear to you, make assumptions in the comment.
+*/
