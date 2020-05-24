6c70c1bdde76d47708ac0b5408d5cb10964ad9d7
berenice bourgon <berenice.bourgon@gmail.com>
2020-05-24 11:49:32 +0200

    Clean repo

--- kata_resolution.js
+++ kata_resolution.js
@@ -1,16 +1,3 @@
-/* 
-Write a test suite then a code in Javascript to do this task:
-
-Simplify a directory path from a string as input.
-Example:
-Input : /a/./b/../../c/
-Output : /c
-
-Write a README to show us how to run your tests.
-
-If something isn't clear to you, make assumptions in the comment.
-*/
-
 const PathSimplifier = {
   simplifiesPath(inputString) {
     // I simplify the input string and turn it into an array:
--- test/kata_resolution_test.js
+++ test/kata_resolution_test.js
@@ -1,32 +1,20 @@
 /* 
-Write a test suite then a code in Javascript to do this task:
-
-Simplify a directory path from a string as input.
-Example:
-Input : /a/./b/../../c/
-Output : /c
-
-Write a README to show us how to run your tests.
-
-If something isn't clear to you, make assumptions in the comment.
-*/
-
-/* 
 MY ASSUMPTIONS:
+
 About the input string:
 1) Must be a string
 2) Must not be empty (at least one slash: "/")
 
 About the function:
-1) Two dots in the path ("../" or "..") mean I move one directory level up
-  Two dots x times means I move x levels up (Ex: if x = 2, if have "../.." or "../../")
+1) Two dots in the path ("../") mean I move one directory level up
+  Two dots x times means I move x levels up (Ex: if x = 2, I have "../../")
 
-2) One dot ("." or "./") means I am staying in the current directory
+2) One dot (./") means I am staying in the current directory
 
 3) A slash after a directory name and at the end of the input string is useless and has to be removed
   (Ex: "/myproject/" becomes "/myproject")
 
-4) The output path always begins with a slash (it's an absolute path) but never ends with a slash (a slash after the last directory is useless)
+4) The output path always begins with a slash but never ends with a slash (a slash after the last directory is useless)
 
 5) Multiple consecutive slashes become a single slash
 */
